398f08f9d7579e3ad0598743a9bc1ce420a54520
Glenn Jocher <glenn.jocher@ultralytics.com>
2025-10-24 12:17:56 +0200

    Update __init__.py
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- mobileclip/__init__.py
+++ mobileclip/__init__.py
@@ -1,9 +1,9 @@
 # Ultralytics 🚀 AGPL-3.0 License - https://ultralytics.com/license
 
-#
+
 # For licensing see accompanying LICENSE file.
 # Copyright (C) 2024 Apple Inc. All Rights Reserved.
-#
+
 from __future__ import annotations
 
 import json
